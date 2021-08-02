c902248681af1c6512a21fb15de1dd131390003f
hechengyao <hechengyao@bonc.com.cn>
2021-08-02 20:07:03 +0800

    Commit: WordCountReducer完善

--- src/main/java/com/lanhuigu/bigdata/mapreduce/wordcount/WordCountReducer.java
+++ src/main/java/com/lanhuigu/bigdata/mapreduce/wordcount/WordCountReducer.java
@@ -6,5 +6,12 @@ import org.apache.hadoop.mapreduce.Reducer;
  * @author: HeChengyao
  * @date: 2021/8/2 19:35
  */
+
+/**
+ * KEYIN, reduce阶段输入的key的类型：Text
+ * VALUEIN, reduce阶段输入的value的类型：IntWritable
+ * KEYOUT, reduce阶段输出的key类型：Text
+ * VALUEOUT, reduce阶段输出的value类型：IntWritable
+ */
 public class WordCountReducer extends Reducer {
 }
