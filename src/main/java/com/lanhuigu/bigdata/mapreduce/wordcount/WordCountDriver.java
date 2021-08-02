cdb567dec5cf19a2e908461455b3a8fd2be18a69
hechengyao <hechengyao@bonc.com.cn>
2021-08-02 19:59:43 +0800

    Initial commit

---
+++ src/main/java/com/lanhuigu/bigdata/mapreduce/wordcount/WordCountDriver.java
@@ -0,0 +1,8 @@
+package com.lanhuigu.bigdata.mapreduce.wordcount;
+
+/**
+ * @author: HeChengyao
+ * @date: 2021/8/2 19:35
+ */
+public class WordCountDriver {
+}
---
+++ src/main/java/com/lanhuigu/bigdata/mapreduce/wordcount/WordCountReducer.java
@@ -0,0 +1,10 @@
+package com.lanhuigu.bigdata.mapreduce.wordcount;
+
+import org.apache.hadoop.mapreduce.Reducer;
+
+/**
+ * @author: HeChengyao
+ * @date: 2021/8/2 19:35
+ */
+public class WordCountReducer extends Reducer {
+}
